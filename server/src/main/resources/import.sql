e4aa46c0921aa5ff7e5a0039baec853c3669707e
Miralhas <victor2.miralhas@gmail.com>
2026-01-02 23:26:17 -0300

    feat: muscle groups entity

--- server/src/main/resources/import.sql
+++ server/src/main/resources/import.sql
@@ -4,3 +4,17 @@ insert into users(email, username, created_at, password) VALUES ('admin@admin.co
 
 insert into user_roles(role_id, user_id) VALUES (1,1);
 insert into user_roles(role_id, user_id) VALUES (2,1);
+
+INSERT INTO muscle_groups(name, slug) VALUES ('Chest', 'chest');
+INSERT INTO muscle_groups(name, slug) VALUES ('Back', 'back');
+INSERT INTO muscle_groups(name, slug) VALUES ('Shoulders', 'shoulders');
+INSERT INTO muscle_groups(name, slug) VALUES ('Biceps', 'biceps');
+INSERT INTO muscle_groups(name, slug) VALUES ('Triceps', 'triceps');
+INSERT INTO muscle_groups(name, slug) VALUES ('Forearms', 'forearms');
+INSERT INTO muscle_groups(name, slug) VALUES ('Abdominals', 'abdominals');
+INSERT INTO muscle_groups(name, slug) VALUES ('Obliques', 'obliques');
+INSERT INTO muscle_groups(name, slug) VALUES ('Lower Back', 'lower-back');
+INSERT INTO muscle_groups(name, slug) VALUES ('Glutes', 'glutes');
+INSERT INTO muscle_groups(name, slug) VALUES ('Quadriceps', 'quadriceps');
+INSERT INTO muscle_groups(name, slug) VALUES ('Hamstrings', 'hamstrings');
+INSERT INTO muscle_groups(name, slug) VALUES ('Calves', 'calves');
