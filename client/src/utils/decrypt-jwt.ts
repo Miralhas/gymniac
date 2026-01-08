7a00aa52350402682102c343379b0f45a9975bc6
Miralhas <victor2.miralhas@gmail.com>
2026-01-08 18:02:43 -0300

    feat: remove cache() from decrypt function

--- client/src/utils/decrypt-jwt.ts
+++ client/src/utils/decrypt-jwt.ts
@@ -1,13 +1,10 @@
 import { CustomJwtPayload } from "@/types/auth";
 import { jwtDecode } from "jwt-decode";
-import { cache } from "react";
 
-const decodeJwt = (token?: string) => {
+export const decrypt = (token?: string) => {
   if (!token) return null;
   const decoded = jwtDecode(token) as CustomJwtPayload;
   if (!decoded.user) return null;
   return decoded.user;
 }
-
-export const decrypt = cache(decodeJwt)
 
