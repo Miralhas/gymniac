1c555d5650a1dff0c92291ff1e18ca70c369d1c2
Miralhas <victor2.miralhas@gmail.com>
2026-05-24 09:11:06 -0300

    fix: wrong props

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -10,7 +10,7 @@ const LoginPage = async ({ searchParams }: { searchParams: Promise<{ [key: strin
 
   return (
     <div className="w-full mx-auto max-w-100 space-y-4 relative">
-      <LoginForm redirectUri={params["redirect"]} />
+      <LoginForm redirectAfter={params["redirect"]} />
       <div className="absolute -top-20 -left-10 w-[350px] h-[350px] bg-emerald-600/40 rounded-full blur-[80px] opacity-70"></div>
       <div className="absolute top-40 left-40 w-[350px] h-[350px] bg-emerald-600/30 rounded-full blur-[70px] opacity-50"></div>
     </div>
